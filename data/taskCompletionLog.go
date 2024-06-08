81b7582385084d1da6389a7a52cdbb4178cfb49e
Francisco Javier González del Solar <frangdelsolar@gmail.com>
2024-06-08 17:14:00 -0300

    pending stuff

--- data/taskCompletionLog.go
+++ data/taskCompletionLog.go
@@ -45,34 +45,21 @@ func GetTaskCompletionLogsByTaskId(taskId string) []models.TaskCompletionLog {
 	return tasks
 }
 
-type PendingTCLContract struct {
-	taskID        string
-	taskGoalID    string
-	dueDate       time.Time
-	Label         string
-}
-
 type GoalsContract struct {
 	TaskGoalID string
 	TaskID string
 	StartDate time.Time
 	EndDate time.Time
 	Label string
-	Frequency models.TaskFrequency
 	Category models.TaskGoalCategory
+	FrequencyID string
+	FrequencyType string
+	FrequencyDay int
+	FrequencyMonth int
+	FrequencyDayOfWeek int
 }
 
-type CompletionContract struct {
-	TaskGoalID string
-	TaskID string
-	CompletedAt time.Time	
-}
-
-// refDate is the date where the query is done. It will limit the results up to that date.
-func GetPendingTaskCompletionLogs(refDate time.Time) []PendingTCLContract {
-	const limit = 10
-	var tcls []PendingTCLContract
-
+func getGoalsData(refDate time.Time) []GoalsContract {
 	// Get goals for reference date
 	var taskGoals []GoalsContract
 
@@ -82,11 +69,16 @@ func GetPendingTaskCompletionLogs(refDate time.Time) []PendingTCLContract {
 					task_goals.start_date as StartDate,
 					task_goals.end_date as EndDate,
 					task_goals.category as Category,
-					task_goals.frequency as Frequency,
 					tasks.id as TaskID, 
-					tasks.title as Label`,
+					tasks.title as Label,
+					task_frequencies.id as FrequencyID,
+					task_frequencies.type as FrequencyType,
+					task_frequencies.day as FrequencyDay,
+					task_frequencies.month as FrequencyMonth,
+					task_frequencies.day_of_week as FrequencyDayOfWeek`,
 				).
 		Joins("join tasks on task_goals.task_id = tasks.id").
+		Joins("join task_frequencies on task_goals.frequency_id = task_frequencies.id").
 		Where(`
 				task_goals.start_date <= ? AND 
 					(
@@ -96,56 +88,107 @@ func GetPendingTaskCompletionLogs(refDate time.Time) []PendingTCLContract {
 			  `, refDate, refDate, time.Time{}).
 		Find(&taskGoals)
 
+	return taskGoals
+}
+
+type CompletionContract struct {
+	TaskGoalID string
+	TaskID string
+	CompletedAt time.Time	
+}
+func getCompletionLogsByTaskGoalAndDate(refDate time.Time, tg GoalsContract) []CompletionContract{
+	var completionLogs []CompletionContract
+
+	DB.Table("task_completion_logs").
+		Select(`DISTINCT
+					task_completion_logs.task_id as TaskID,
+					task_completion_logs.task_goal_id as TaskGoalID,
+					task_completion_logs.completed_at as CompletedAt`,
+				).
+		Where(
+			`task_completion_logs.task_goal_id = ? AND
+				task_completion_logs.completed_at <= ? AND
+				task_completion_logs.completed_at >= ?`,
+			tg.TaskGoalID, refDate, tg.StartDate,
+		).
+		Order("task_completion_logs.completed_at desc").
+		Find(&completionLogs)
+
+	return completionLogs
+}
+
+
+type PendingTCLContract struct {
+	taskID        string
+	taskGoalID    string
+	dueDate       time.Time
+	Label         string
+}
+
+// refDate is the date where the query is done. It will limit the results up to that date.
+func GetPendingTaskCompletionLogs(refDate time.Time) []PendingTCLContract {
+	const limit = 100
+	var tcls = []PendingTCLContract{}
+
+	taskGoals := getGoalsData(refDate)
 	log.Debug().Interface("taskGoals", taskGoals).Msg("Task Goals")
 
-
-	
 	for _, tg := range taskGoals {
-		var completionLogs []CompletionContract
-
-		DB.Table("task_completion_logs").
-			Select(`DISTINCT
-						task_completion_logs.task_id as TaskID,
-						task_completion_logs.task_goal_id as TaskGoalID,
-						task_completion_logs.completed_at as CompletedAt`,
-					).
-			Where(
-				`task_completion_logs.task_goal_id = ? AND
-					task_completion_logs.completed_at <= ? AND
-					task_completion_logs.completed_at >= ?`,
-				tg.TaskGoalID, refDate, tg.StartDate,
-			).
-			Order("task_completion_logs.completed_at desc").
-			Find(&completionLogs)
-
+		
+		completionLogs := getCompletionLogsByTaskGoalAndDate(refDate, tg)
 		log.Debug().Interface("completionLogs", completionLogs).Msg("Completion Logs")
-
+		
 		// create a map with date as key
-		var dates = make(map[time.Time]CompletionContract)
+		var dates = []time.Time{}
 
 		for _, cl := range completionLogs {
-			dates[cl.CompletedAt] = cl
+			dates = append(dates, cl.CompletedAt)
 		}
 
 		// Traverse time according to frquency type and create pendign tasks
+		if tg.FrequencyType == string(models.Daily) {
+			log.Debug().Msg("Do somehting about daily")
+		} else if tg.FrequencyType == string(models.Weekly) {
+			log.Debug().Msg("Do somehting about weekly")
+		} else if tg.FrequencyType == string(models.Monthly) {
+			log.Debug().Msg("Do something about monthly")
 
-		// if tg.Frequency == models.Daily {
-		// 	log.Debug().Msg("Do somehting about daily")
-		// } else if tg.Frequency == models.Weekly {
-		// 	log.Debug().Msg("Do somehting about weekly")
-		// } else if tg.Frequency == models.Monthly {
-		// 	log.Debug().Msg("Do somehting about monthly")
+			// While nextDate is >= tg.StartDate or Limit is reached
 
-		// 	for i := 0; i < limit; i++ {
 
-		// 	}
+			for i := 0; i < limit; i++ {
+				nextDate := refDate.AddDate(0, -i, 0)
+				if nextDate.Before(tg.StartDate) {
+					break
+				}
+				log.Debug().Msgf("RefDate: %s, NextDate: %s", refDate.Format("2006-01-02"), nextDate.Format("2006-01-02"))
 
-		// } else if tg.Frequency == models.Yearly {
-		// 	log.Debug().Msg("Do somehting about yearly")
-		// }
+				for _, d := range dates {
+					if d.Month() == nextDate.Month() {
+						log.Debug().Msgf("Matched %s. There's a completion log for this date:", d.Format("2006-01-02"))
+						
+					}
+				} 
+				
+				tcl:= PendingTCLContract{
+					taskID: tg.TaskID,
+					taskGoalID: tg.TaskGoalID,
+					dueDate: nextDate,
+					Label: tg.Label,
+				}
+				tcls = append(tcls, tcl)
+			}
+
+
+		} else if tg.FrequencyType == string(models.Yearly) {
+			log.Debug().Msg("Do somehting about yearly")
+		}
+
+
+
 	}
 
-
+	log.Debug().Interface("tcls", tcls).Msg("tcls")
 	return tcls
 }
 
