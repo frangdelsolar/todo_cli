33f8b85a6abff65c9bc257c7dca2399318ebc0ac
Francisco Javier González del Solar <frangdelsolar@gmail.com>
2024-06-10 21:49:29 -0300

    Currency v1.0.0

---
+++ pkg/currency/db.go
@@ -0,0 +1,83 @@
+package currency
+
+import (
+	"fmt"
+)
+
+// CreateCurrency creates a new Currency object in the database.
+//
+// Parameters:
+// - currencyCode: the code of the currency (e.g. "USD", "EUR").
+// - amount: the amount of the currency.
+// - exchangeDate: the date of the exchange rate.
+//
+// Returns:
+// - *Currency: the created Currency object.
+// - error: an error if the creation failed.
+func CreateCurrency(currencyCode string, amount string, exchangeDate string) (*Currency, error) {
+	var c *Currency
+
+	c, err := NewCurrency(currencyCode, amount, exchangeDate)
+	if err != nil {
+		return c, err
+	}
+
+	db.Create(&c)
+	
+	return c, nil
+}
+
+// GetCurrencyById retrieves a Currency object from the database by its ID.
+//
+// Parameters:
+// - id: the ID of the Currency to retrieve.
+//
+// Returns:
+// - Currency: the retrieved Currency object, or an empty Currency object if not found.
+// - error: an error if the Currency retrieval fails.
+func GetCurrencyById(id string) (Currency, error) {
+	var c Currency
+
+	db.First(&c, "id = ?", id)
+	if c == (Currency{}) {
+		return c, fmt.Errorf("currency with ID %s not found", fmt.Sprint(id))
+	}
+	return c, nil
+}
+
+
+// GetAllCurrencies retrieves all the currencies from the database.
+//
+// Returns:
+// - []Currency: a slice of Currency objects representing all the currencies.
+func GetAllCurrencies() []Currency {
+	var cs []Currency
+
+	db.Find(&cs)
+
+	if len(cs) == 0 {
+		log.Warn().Msg("No currencies found")
+		return cs
+	}
+
+	return cs
+}
+
+// DeleteCurrency deletes a currency from the database by its ID.
+//
+// Parameters:
+// - id: the ID of the currency to delete.
+//
+// Returns:
+// - error: an error if the currency retrieval or deletion fails.
+func DeleteCurrency(id string) error {
+	c, err := GetCurrencyById(id)
+	if err != nil {
+		log.Err(err).Msg("Error getting currency")
+		return err
+	}
+
+	db.Delete(&c)
+
+	return nil
+}
--- pkg/currency/go.mod
+++ pkg/currency/go.mod
@@ -5,7 +5,6 @@ go 1.22.4
 require (
 	github.com/frangdelsolar/todo_cli/pkg/data v1.0.0
 	github.com/frangdelsolar/todo_cli/pkg/logger v1.0.3
-	github.com/rs/zerolog v1.33.0
 	gorm.io/gorm v1.25.10
 )
 
@@ -15,6 +14,7 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mattn/go-sqlite3 v1.14.17 // indirect
+	github.com/rs/zerolog v1.33.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
 	gorm.io/driver/sqlite v1.5.5 // indirect
 )
