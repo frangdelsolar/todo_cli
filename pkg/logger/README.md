30af0755294e0df557a8cd1e386513b1a4cdf364
Francisco Javier González del Solar <frangdelsolar@gmail.com>
2024-06-21 17:40:06 -0300

    logger v1.1.2

--- pkg/logger/README.md
+++ pkg/logger/README.md
@@ -1,4 +1,4 @@
-# Logger v1.1.1
+# Logger v1.1.2
 
 The `logger` package provides a wrapper around the popular zerolog logging library (https://github.com/rs/zerolog) with additional context specific to your TODO application. It provides a wrapper around zerolog, adding context specific to your application and a simple interface for managing the logger instance.
 
@@ -7,7 +7,9 @@ The `logger` package provides a wrapper around the popular zerolog logging libra
 -   Leverages zerolog for structured logging.
 -   Adds context for your TODO application (e.g., app name, version).
 -   Provides functions to create, configure, and retrieve the logger instance.
--   Integrates with your configuration package (config) to retrieve the log level from environment variables.
+-   Integrates with your configuration package (`config`) to retrieve the log level from environment variables.
+-   Writes logs to a file named `<pkgName>.log` or `default.log` in a dedicated logs directory.
+-   Offers a console-like format for log files, including timestamps and caller information.
 
 ## Installation
 
@@ -29,7 +31,7 @@ require github.com/frangdelsolar/todo_cli/pkg/logger <VERSION>
 
 ### 1. Configure Logging Level on your environment
 
--   Set the desired log level (e.g., "debug", "info", "warn", "error") using an environment variable.
+-   Set the desired log level (e.g., "trace", "debug", "info", "warn", "error") using an environment variable.
 -   Refer to `config` package (documentation)[https://github.com/frangdelsolar/todo_cli/blob/master/pkg/config/README.md] to get instructions on how to approach this.
 
 ### 2. Create the logger instance
@@ -49,9 +51,13 @@ require github.com/frangdelsolar/todo_cli/pkg/logger <VERSION>
             panic(err)
         }
 
-        log := logger.NewLogger(APP_NAME, APP_VERSION)
+        log := logger.NewLogger(logger.LoggerConfig{
+            PackageName: APP_NAME,
+            PackageVersion: APP_VERSION,
+            LogLevel: cfg.LogLevel, // this can either be define here or in the env file.
+        })
 
-        log.Info().Msgf("Running %s environment ", cfg.AppEnv)
+        log.Info().Msgf("Running %s environment", cfg.AppEnv)
         log.Info().Msg("Running TODO APP v" + APP_VERSION)
     }
 ```
@@ -59,8 +65,8 @@ require github.com/frangdelsolar/todo_cli/pkg/logger <VERSION>
 **Output example**
 
 ```shell
-    4:08PM INF Running local environment  app="TODO APP v1.0.4"
-    4:07PM INF Running TODO APP v1.0.4 app="TODO APP v1.0.4"
+    17:31:37 INF cmd/main.go:23 > Running local environment app="TODO APP v1.0.4"
+    17:31:38 INF cmd/main.go:24 > Running TODO APP v1.0.4 app="TODO APP v1.0.4"
 ```
 
 ### 3. Get the Logger:
@@ -71,6 +77,7 @@ require github.com/frangdelsolar/todo_cli/pkg/logger <VERSION>
     appLogger := logger.GetLogger()
     appLogger.Info().Msg("Logging with retrieved logger instance")
 ```
+Note: if there is no logger already initialized, it will return a default instance.
 
 ## Documentation
 
--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -3,16 +3,24 @@ package logger
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"strconv"
 
 	"github.com/frangdelsolar/todo_cli/pkg/config"
 	"github.com/rs/zerolog"
 )
 
-const PKG_NAME = "Logger PKG"
-const PKG_VERSION = "1.1.1"
+const (
+    PKG_NAME      = "Logger PKG"
+    PKG_VERSION   = "1.1.2"
+    defaultLogFile = "default.log"
+    logsDir        = "logs"
+    defaultLogLevel = zerolog.DebugLevel
+)
 
-var log *Logger
-var cfg *config.Config
+var (
+    log *Logger
+)
 
 type Logger struct {
 	*zerolog.Logger
@@ -25,46 +33,106 @@ type Logger struct {
 //
 // Returns:
 // - A pointer to the newly created Logger instance.
-func NewLogger(pkgName string, pkgVersion string) *Logger {
 
-	cfg = config.GetConfig()
+type LoggerConfig struct {
+    PackageName string
+    PackageVersion string
+    LogLevel string
+    LogFile string
+}
 
-	logger := ConfigLogger(cfg.LogLevel, pkgName, pkgVersion)
+// GetZeroLevel returns the zero log level based on the logger configuration.
+//
+// It takes no parameters and returns a zerolog.Level.
+func (lc LoggerConfig) GetZeroLevel() zerolog.Level {
+    level, err := zerolog.ParseLevel(lc.LogLevel)
+    if err != nil {
+        level = defaultLogLevel
+    }
+    return level
+}
+
+type consoleFormatter struct {
+  zerolog.ConsoleWriter
+}
+
+
+// GetZeroLogger returns a zerolog.Logger instance with the specified logging level and output configuration.
+//
+// Parameters:
+// - lc: A LoggerConfig struct containing the configuration for the logger.
+//
+// Returns:
+// - A zerolog.Logger instance with the specified logging level and output configuration.
+func (lc LoggerConfig) GetZeroLogger() zerolog.Logger {
+	// Set Loggging Level
+    zerolog.SetGlobalLevel(lc.GetZeroLevel())
+    
+    // Set Caller -> This will show the file name and line number in the log output
+    zerolog.CallerMarshalFunc = func(pc uintptr, file string, line int) string {
+        path := filepath.Dir(file)
+        file = filepath.Base(path) + "/" + filepath.Base(file)
+        return file + ":" + strconv.Itoa(line)
+    }
+
+    runLogFile, _ := os.OpenFile(
+        lc.LogFile,
+        os.O_APPEND|os.O_CREATE|os.O_WRONLY,
+        0664,
+    )
+    
+    // This are the writters that will be used in the logger: console and file
+    multi := zerolog.MultiLevelWriter(zerolog.ConsoleWriter{
+        Out: os.Stdout,
+        TimeFormat: "15:04:05",
+    }, runLogFile)
+	
+    return zerolog.New(multi).
+		With().
+        Caller().
+		Timestamp().
+		Str("app", fmt.Sprintf("%s v%s", lc.PackageName, lc.PackageVersion)).
+		Logger()
+}
+
+func NewLogger(cfg LoggerConfig) *Logger {
+    // Define defaults
+
+    // Load Environment Variables
+	appConfig := config.GetConfig()
+
+    // Set default log level if not provided either in config or environment
+    if cfg.LogLevel == "" {
+        if appConfig != nil && appConfig.LogLevel == "" {
+            cfg.LogLevel = appConfig.LogLevel
+        } else {
+            cfg.LogLevel = defaultLogLevel.String()
+        }
+    }
+
+    // Define file
+    if cfg.LogFile == "" {
+        if cfg.PackageName != "" {
+            cfg.LogFile = fmt.Sprintf("%s.log", cfg.PackageName)
+        } else {
+            cfg.LogFile = defaultLogFile
+        }
+    }
+
+	logger := cfg.GetZeroLogger()
 	log = &Logger{&logger}
 	return log
 }
 
-func ConfigLogger(logLevel string, pkgName string, pkgVersion string) zerolog.Logger {
-	var zlogLevel zerolog.Level
-	switch logLevel {
-    case "trace":
-        zlogLevel = zerolog.TraceLevel
-	case "debug":
-		zlogLevel = zerolog.DebugLevel
-	case "info":
-		zlogLevel = zerolog.InfoLevel
-	case "warn":
-		zlogLevel = zerolog.WarnLevel
-	case "error":
-		zlogLevel = zerolog.ErrorLevel
-	default:
-		zlogLevel = zerolog.DebugLevel
-	}
-
-	zerolog.SetGlobalLevel(zlogLevel)
-	zl := zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr}).
-		With().
-		Timestamp().
-		Str("app", fmt.Sprintf("%s v%s", pkgName, pkgVersion)).
-		Logger()
-
-	return zl
-}
 
 // GetLogger returns a pointer to the Logger instance.
 //
 // Returns:
 // - *Logger: A pointer to the Logger instance.
 func GetLogger() *Logger {
+    if log == nil {
+        log = NewLogger(LoggerConfig{})
+    }
+
 	return log
 }
