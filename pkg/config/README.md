d215ad218d912e0201b824b1506db4454dab0ae3
Francisco Javier González del Solar <frangdelsolar@gmail.com>
2024-06-21 21:35:39 -0300

    config bump

--- pkg/config/README.md
+++ pkg/config/README.md
@@ -1,6 +1,6 @@
-# Config PKG v1.0.4
+# Config PKG v1.0.5
 
-This package provides a centralized and structured approach to managing environment variables in your Go applications. It offers functionality for loading environment variables from files and accessing them throughout your code.
+This package provides a centralized and structured approach to managing environment variables in your Go applications. It offers functionality for loading environment variables from files and accessing them throughout your code, and now includes features for managing session data.
 
 ## Features
 
@@ -8,6 +8,7 @@ This package provides a centralized and structured approach to managing environm
 -   Supports different .env files for various environments (e.g., dev, prod).
 -   Provides a Config struct to hold loaded environment variables.
 -   Offers functions to load and access the configuration.
+-   Introduces functionalities for managing session variables.
 
 ## Installation
 
@@ -68,6 +69,59 @@ This package provides a centralized and structured approach to managing environm
 ```
 
 2. Update `.env` Files: Add the new variable to the appropriate `.env` files (e.g., `.env`, `.env.dev`, `.env.prod`) with its corresponding value.
+3. Update `SetSession()` method:
+```go
+    // Dump config and session to .env file
+    storedKey := fmt.Sprintf("%s%s", sessionVariablesPrefix, key)
+    err := godotenv.Write(map[string]string{
+        "PORT": c.Port,
+        "NEW_VAR": c.NewVar, <- Add this
+        // ... other environment variables
+        storedKey: value,
+    }, c.envFile)
+```
+4. Update `Load()` method:
+```go 
+	// Load environment variables from the file
+	err = godotenv.Load(filePath)
+	if err != nil {
+		fmt.Errorf("error loading %s file", filePath)
+		return nil, err
+	}
+
+    config.Port = os.Getenv("PORT")
+	config.NewVar = os.Getenv("NEW_VAR") <- Add this
+    // ... other environment variables
+```
+
+## Adding New Session Variables
+The `Config` struct now includes a `Session` field, which is a `map[string][string]`. You can use this map to store and retrieve session specific key-value pairs.
+1. **Set Session Variables**:
+- Use the `SetSession` method of the `Config` struct to set a session variable.
+```go
+    err := cfg.SetSession("user_id", "12345")
+    if err != nil {
+        // handle error
+    }
+```
+2. **Get Session Variables**:
+- Use the `GetSession` method of the `Config` struct to retrieve a session variable by its key.
+```go
+    userId, err := cfg.GetSession("user_id")
+    if err != nil {
+        // handle error (e.g., key not found)
+    } else {
+        fmt.Println("User ID:", userId)
+    }
+```
+**Note**: This variables will be stored in your `.env` file with the set prefix in the `config.go` file.
+```go
+    const sessionVariablesPrefix = "TODO_APP_SESSION_"
+```
+The variable created as `user_id` will be stored as:
+```
+    TODO_APP_SESSION_USER_ID=1
+```
 
 ### Optional: Environment-Specific Files
 
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -4,48 +4,119 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/joho/godotenv"
 )
 
-var PKG_NAME = "Config PKG"
-var PKG_VERSION = "1.0.4"
+const (
+    PKG_NAME = "Config PKG"
+    PKG_VERSION = "1.0.5"
+
+    sessionVariablesPrefix = "TODO_APP_SESSION_"
+)
 
 var config *Config
 
 // Config represents the configuration loaded from environment variables
 type Config struct {
-	AppEnv   string
-	LogLevel string
-	DBPath   string
-    FirebaseAdminSdk string
+	AppEnv   string `env:"APP_ENV" default:"dev"`
+	LogLevel string `env:"LOG_LEVEL" default:"debug"`
+	DBPath   string `env:"DB_PATH" default:"./data.db"`
+    FirebaseAdminSdk string `env:"FIREBASE_ADMIN_SDK" default:""`
+    Session map[string]string
+    envFile string
 }
 
-// Load reads configuration from environment variables based on the APP_ENV
+// SetEnvFile sets the environment file based on the AppEnv field of the Config struct.
+//
+// The AppEnv field is used to determine the value of the envFile field.
+// If the AppEnv field is "dev", the envFile field is set to ".env.dev".
+// If the AppEnv field is "prod", the envFile field is set to ".env.prod".
+// If the AppEnv field is "test", the envFile field is set to ".env.test".
+// Otherwise, the envFile field is set to ".env".
+func (c *Config) SetEnvFile() {
+	c.envFile = ".env"
+	if c.AppEnv == "dev" {
+		c.envFile = ".env.dev"
+	} else if c.AppEnv == "prod" {
+		c.envFile = ".env.prod"
+	} else if c.AppEnv == "test" {
+		c.envFile = ".env.test"
+	}
+}
+
+// SetSession sets a session variable in the Config struct and writes the updated configuration and session to the .env file.
+//
+// Parameters:
+// - key: the key of the session variable to be set.
+// - value: the value of the session variable to be set.
+//
+// Returns:
+// - error: an error if there was a problem writing the updated configuration and session to the .env file.
+func (c *Config) SetSession(key string, value string) error {
+    c.Session[key] = value
+
+    // Dump config and session to .env file
+    storedKey := fmt.Sprintf("%s%s", sessionVariablesPrefix, key)
+    err := godotenv.Write(map[string]string{
+        "LOG_LEVEL": c.LogLevel,
+        "DB_PATH": c.DBPath,
+        "FIREBASE_ADMIN_SDK": c.FirebaseAdminSdk,
+        storedKey: value,
+    }, c.envFile)
+    
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+// GetSession retrieves a session variable from the Config struct using the provided key.
+//
+// Parameters:
+// - key: the key of the session variable to be retrieved.
+// Return types:
+// - string: the value of the session variable.
+// - error: an error if the key is not found in the session.
+func (c *Config) GetSession(key string) (string, error) {
+    // storedKey := fmt.Sprintf("%s%s", sessionVariablesPrefix, key)
+    value, ok := c.Session[key]
+    if !ok {
+        return "", fmt.Errorf("key %s not found in session", key)
+    }
+    return value, nil
+}
+
+// Load loads the configuration values from environment variables and returns a Config struct and an error.
+//
+// It sets the configuration values from environment variables, including APP_ENV, LOG_LEVEL, DB_PATH, and FIREBASE_ADMIN_SDK.
+// It also sets session variables from environment variables that start with sessionVariablesPrefix.
+//
+// Parameters:
+//   None.
+//
+// Return types:
+//   *Config: A pointer to the Config struct with the loaded configuration values.
+//   error: An error if there was a problem loading the configuration values.
 func Load() (*Config, error) {
 
 	config = &Config{}
 	var err error
 
-	env := os.Getenv("APP_ENV")
-
-	// Determine the appropriate environment file based on APP_ENV
-	envFile := ".env"
-	if env == "dev" {
-		envFile = ".env.dev"
-	} else if env == "prod" {
-		envFile = ".env.prod"
-	} else if env == "test" {
-		envFile = ".env.test"
-	}
+    // Set configuration values from environment variables
+	config.AppEnv = os.Getenv("APP_ENV")
+    config.SetEnvFile()
 
 	// Get the current working directory
 	currentDir, err := os.Getwd()
 	if err != nil {
-		panic(err)
+        fmt.Errorf("error getting current directory")
+        return nil, err
 	}
 
-	filePath := filepath.Join(currentDir, envFile)
+	filePath := filepath.Join(currentDir, config.envFile)
 
 	// Load environment variables from the file
 	err = godotenv.Load(filePath)
@@ -54,11 +125,22 @@ func Load() (*Config, error) {
 		return nil, err
 	}
 
-	// Set configuration values from environment variables
-	config.AppEnv = env
-	config.LogLevel = os.Getenv("LOG_LEVEL")
+    config.LogLevel = os.Getenv("LOG_LEVEL")
 	config.DBPath = os.Getenv("DB_PATH")
     config.FirebaseAdminSdk = os.Getenv("FIREBASE_ADMIN_SDK")
+
+    // Set session variables
+    config.Session = make(map[string]string)
+    for _, value := range os.Environ() {
+        if strings.HasPrefix(value, sessionVariablesPrefix) {
+            keyValue := strings.TrimPrefix(value, sessionVariablesPrefix)
+
+            key := strings.Split(keyValue, "=")[0]
+            value := strings.Split(keyValue, "=")[1]
+
+            config.Session[key] = value
+        }
+    }
 
 	return config, nil
 }
